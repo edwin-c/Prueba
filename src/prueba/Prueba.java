3a9b879399d921f00ebd7ca824ca698e18729283
Edwin Chocoy <Edwin Chocoy@DESKTOP-P6M354R>
2017-09-11 11:19:31 -0600

    innit commit

---
+++ src/prueba/Prueba.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba;
+
+/**
+ *
+ * @author Edwin Chocoy
+ */
+public class Prueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //https://github.com/edwin-c/Prueba.git
+        //master
+    }
+    
+}
